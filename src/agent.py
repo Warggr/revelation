0aa9b421a99160396445a6458a745d3b13469728
Pierre Ballif <pierre.ballif@tum.de>
2022-07-02 16:12:30 +0200

    Create a progress bar

--- src/agent.py
+++ src/agent.py
@@ -3,6 +3,7 @@ from enum import Enum, unique
 from constants import MAX_RESOURCES, MAX_ACTIONS, Timestep, Direction, FULL_BOARD_WIDTH
 from card import ActionCard
 from character import Character
+import sys
 
 import math
 
@@ -158,7 +159,8 @@ class SearchAgent(Agent):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.plans = []
-    def evaluateStep(self, myId, oldState, step):
+    @staticmethod
+    def evaluateStep(myId, oldState, step):
         if step.type == 'atk':
             lostHP = step.kwargs[ 'lostLife' ]
             minDistance = 30
@@ -198,38 +200,53 @@ class SearchAgent(Agent):
     def onBegin(self, state : 'State'):
         #print('----ON BEGIN----')
         assert state.iActive == self.myId
-        (state, decisions, heuristic) = self.planAhead( state, 0 )
+        (state, decisions, heuristic) = self.planAhead( state,  [[[[]]]] )
         #print('Found', state, decisions, heuristic)
         #decision = [ dec for dec in decisions if isinstance(dec, ActionDecision) ][0]
         #print('ActionDecision:', decision.card)
         self.plans = decisions
-    def planAhead(self, state : 'State', maxDepth : int):
-        #print('Starting minmax with nbPaths = 0')
+    @staticmethod
+    def planAhead(state : 'State', maxDepth, indent = ''):
+        """
+        Constructs all descendants of @param state. up to a level defined by @param maxDepth.
+        Selects the one that is best for the active player (defined by state.iActive)
+        Returns a tuple (state, decisions, heuristic) with the bext future state, the decisions that led to it, and the heuristic of that state.
+
+        For param maxDepth:
+        - [] indicates that only my own turn will be considered
+        - [ X ] my own turn will be considered, then the opponent will choose their best state with maxDepth=X
+        - [ X, Y ] I will simulate my own turn, then the opponent will simulate their turn with maxDepth=X and return the result, then I will simulate the consequences of their turn with maxDepth=Y.
+
+        For example, for a full-2-turns-ahead simulation, use maxDepth = [ [ [ [] ] ] ]
+        """
+        #print(indent, 'Starting planAhead with depth', maxDepth)
         nbPaths = 0
         maxHeur = None
         maxHeurTemp = -20
         minTolerated = -20
         bestMoves = None
         bestState = None
-        stack = [ (state, [], 0, 0) ] #state, decision history, heuristic, depth
+        progress = []
+        stack = [ (state, [], 0, maxDepth, False) ] #state, decision history, heuristic, depth, second-pass
         while stack: #not empty
-            active = stack.pop()
-            state = active[0]
-
-            #print('nbPaths is', nbPaths)
-            #if nbPaths > 10000:
-                #raise Exception()
-            #print("unloading step of type ", state.timestep, '@', active[3])
-            #print('maxHeur is', maxHeur)
-#            print('with cards:', [ len(i.actionDeck.cards) for i in state.players ])
-#            print( 'History', active[1] )
+            (state, ac_decisionhistory, ac_heuristic, ac_depth, pass2) = stack.pop()
+            #print()
+            #print(indent, state.timestep, progress, pass2)
+            if pass2:
+                if state.timestep == Timestep.DISCARDED or state.timestep == Timestep.ABILITYCHOSEN:
+                    sys.stdout.write('\b' * 13) # deleting my progress-frame
+                    progress.pop()
+                    sys.stdout.flush()
+                continue
+            else:
+                stack.append((state, None, None, None, True))
 
             if state.timestep == Timestep.BEGIN:
                 decision = ActionOrResource.ACTION
                 (newState, step) = state.stepDraw( decision )
-                stack.append( (newState, active[1] + [ decision ], active[2] + self.evaluateStep( state.iActive, state, step ), active[3] ) )
+                stack.append( (newState, ac_decisionhistory + [ decision ], ac_heuristic + SearchAgent.evaluateStep( state.iActive, state, step ), ac_depth, False ) )
             elif state.timestep == Timestep.DISCARDED:
-                print('Begin allMov phase')
+                #print('Begin allMov phase')
                 allPossibleMoves = {}
                 substack = [ (state, 2, []) ]
                 nbChildrenFiltered = 0
@@ -259,19 +276,26 @@ class SearchAgent(Agent):
                 for bundle in allPossibleMoves.values():
                     (newState, decisions) = bundle
                     heuristic_diff = 0 # TODO add step or state evaluation
-                    stack.append( (newState, active[1] + decisions, active[2] + heuristic_diff, active[3] ) )
-                print('End allMov phase! Same-state filtering kept', len(allPossibleMoves), 'out of', nbChildrenFiltered + len(allPossibleMoves) )
+                    stack.append( (newState, ac_decisionhistory + decisions, ac_heuristic + heuristic_diff, ac_depth, False ) )
+                #print(indent, 'End allMov phase! Same-state filtering kept', len(allPossibleMoves), 'out of', nbChildrenFiltered + len(allPossibleMoves) )
+                sys.stdout.write(f'[{len(allPossibleMoves):4}\\   1]->')
+                sys.stdout.flush()
+                progress.append(0)
 
             elif state.timestep == Timestep.MOVEDlast:
                 decision = AbilityDecision()
                 (newState, step) = state.stepAbil( decision )
-                stack.append( (newState, active[1] + [ decision ], active[2] + self.evaluateStep( state.iActive, state, step ), active[3] ) )
+                stack.append( (newState, ac_decisionhistory + [ decision ], ac_heuristic + SearchAgent.evaluateStep( state.iActive, state, step ), ac_depth, False) )
+                progress[-1] += 1
+                sys.stdout.write('\b' * 7 + f'{progress[-1]:4}]->')
+                sys.stdout.flush()
+
             elif state.timestep == Timestep.ABILITYCHOSEN:
                 (newState, step) = state.stepAct(None)
-                stack.append( (newState, active[1] + [ None ], active[2] + self.evaluateStep( state.iActive, state, step ), active[3]) ) # the "pass" option
+                stack.append( (newState, ac_decisionhistory + [ None ], ac_heuristic + SearchAgent.evaluateStep( state.iActive, state, step ), ac_depth, False) ) # the "pass" option
                 #print('Appending to stack "pass"', len(stack))
                 ret = ActionDecision(None, None, None)
-                #nbChildren = 0
+                nbChildren = 1 # already one child: the 'pass' decision
                 for card in state.players[ state.iActive ].actions:
                     ret.card = card
                     if card == ActionCard.DEFENSE:
@@ -280,9 +304,9 @@ class SearchAgent(Agent):
                                 ret.subjectPos = subject.position
                                 ret.object = None
                                 (newState, step) = state.stepAct( ret )
-                                stack.append( (newState, active[1] + [ ret.copy() ], active[2] + self.evaluateStep( state.iActive, state, step ), active[3]) )
+                                stack.append( (newState, ac_decisionhistory + [ ret.copy() ], ac_heuristic + SearchAgent.evaluateStep( state.iActive, state, step ), ac_depth, False) )
                                 #print('Appending defense to stack', len(stack))
-                                #nbChildren += 1
+                                nbChildren += 1
                     else:
                         allPossibleAttacks = state.allAttacks()
                         for aggressor in allPossibleAttacks:
@@ -290,37 +314,36 @@ class SearchAgent(Agent):
                                 ret.subjectPos = state.aliveUnits[ state.iActive ][ aggressor ].position
                                 ret.objectPos = victim.position
                                 (newState, step) = state.stepAct( ret )
-                                stack.append( (newState, active[1] + [ ret.copy() ], active[2] + self.evaluateStep( state.iActive, state, step ), active[3]) )
+                                stack.append( (newState, ac_decisionhistory + [ ret.copy() ], ac_heuristic + SearchAgent.evaluateStep( state.iActive, state, step ), ac_depth, False) )
                                 #print('Appending attack to stack', len(stack))
-                                #nbChildren += 1
+                                nbChildren += 1
+                sys.stdout.write(f'<{nbChildren:4}\\   1>->')
+                sys.stdout.flush()
+                progress.append(0)
             elif state.timestep == Timestep.ACTED:
+                progress[-1] += 1
+                sys.stdout.write('\b' * 7 + f'{progress[-1]:4}>->')
+                sys.stdout.flush()
                 (newState, step) = state.beginTurn()
-                if active[3] != maxDepth:
-                    if active[2] >= minTolerated:
-                        if active[2] > maxHeurTemp:
-                            maxHeurTemp = active[2]
-                            if minTolerated < maxHeurTemp - 20:
-                                minTolerated = maxHeurTemp - 20
-                        else:
-                            minTolerated += 1
-                        print('MINNING')
-                        #print(len(stack))
-                        #print(active[2], ', tolerate', minTolerated, '-', maxHeurTemp)
-                        (newState, decisions, heuristic) = self.planAhead( newState, 0 )
-                        print('RETURN TO MAXING')
-                        #print(decisions, heuristic)
-                        active = ( newState, active[1], active[2] - heuristic, active[3] + 2 )
-                        if active[3] + 2 <= maxDepth:
-                            stack.append( active )
-                            continue
-                        #else fallthrough
+                if len(ac_depth) != 0:
+                    #print(indent, 'MINNING')
+                    #print(len(stack))
+                    #print(heuristic, ', tolerate', minTolerated, '-', maxHeurTemp)
+                    (newState, decisions, heuristic) = SearchAgent.planAhead( newState, ac_depth[0], indent+'    ' )
+                    #print(indent, 'RETURN TO MAXING')
+                    #print(decisions, heuristic)
+                    if len(ac_depth) == 2:
+                        active = ( newState, ac_decisionhistory, ac_heuristic - heuristic, ac_depth[1], indent+'    ' )
+                        stack.append( active )
+                        continue
+                    #else fallthrough
                 nbPaths += 1
                 #print('One path found! Heuristic:', active[2], ', max', maxHeur)
                 #print('Stacksize', len(stack))
-                if maxHeur is None or active[2] > maxHeur:
+                if maxHeur is None or ac_heuristic > maxHeur:
                     #print('Best path found! Heuristic:', active[2], ', max', maxHeur)
-                    bestMoves = active[1]
+                    bestMoves = ac_decisionhistory
                     bestState = newState
-                    maxHeur = active[2]
-        print(nbPaths, 'possible futures found')
+                    maxHeur = ac_heuristic
+        #print(indent, nbPaths, 'possible futures found')
         return ( bestState, bestMoves, maxHeur )
