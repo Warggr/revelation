e6fa25d77947949bbbdf6ab36dbd22cdc3eb2576
Pierre Ballif <pierre.ballif@tum.de>
2022-09-09 15:48:57 +0200

    Add RandomAgent

--- src/agent.cpp
+++ src/agent.cpp
@@ -9,7 +9,7 @@ std::ostream& operator<<(std::ostream& o, const position& pos){
     return o;
 }
 
-uint input(uint min, uint max){
+uint HumanAgent::input(uint min, uint max){
     while(true) {
         uint iSel; std::cin >> iSel;
         if(min <= iSel and iSel <= max) return iSel;
@@ -21,64 +21,70 @@ const Player& Agent::getMyPlayer(const State& state) const {
     return state.players[ myId ];
 }
 
-HumanAgent::HumanAgent(uint myId) : Agent(myId) {
+HumanAgent::HumanAgent(uint myId) : StepByStepAgent(myId) {
 }
 
-unsigned int HumanAgent::getSpecialAction(const State& state, Effect& effect) {
+unsigned int StepByStepAgent::getSpecialAction(const State& state, Effect& effect) {
     std::vector<std::string> descriptions = effect.getOptions(state);
     for(unsigned int i=0; i<descriptions.size(); i++){
-        std::cout << '[' << i << "]: " << descriptions[i] << '\n';
+        ostream() << '[' << i << "]: " << descriptions[i] << '\n';
     }
     uint iSel = input(0, descriptions.size() - 1);
     return iSel;
 }
 
-const Character& HumanAgent::chooseCharacter(const State& state) const {
-    for(uint i=0; i<NB_CHARACTERS; i++)
-        if(not isDead(state.units[myId][i].get()))
-            std::cout << '[' << i << "]: " << state.units[myId][i]->im.name << '\n';
+const Character& StepByStepAgent::chooseCharacter(const State& state) {
+    const Character* possibleValues[NB_CHARACTERS];
+    unsigned int j = 0;
+    for(uint i=0; i<NB_CHARACTERS; i++){
+        if(not isDead(state.units[myId][i].get())){
+            ostream() << '[' << j << "]: " << state.units[myId][i]->im.name << '\n';
+            possibleValues[j] = state.units[myId][i].get();
+            j += 1;
+        }
+    }
 
-    std::cout << "Enter which character to select: ";
-    uint iSel = input(0, NB_CHARACTERS-1);
-    return *state.units[myId][iSel];
+    ostream() << "Enter which character to select: ";
+    uint iSel = input(1, j) - 1;
+    return *possibleValues[iSel];
 }
 
-ActionOrResource HumanAgent::getDrawAction(const State&) {
-    std::cout << "Choose [1] draw action or [2] draw resource: ";
+ActionOrResource StepByStepAgent::getDrawAction(const State&) {
+    ostream() << "Choose [1] draw action or [2] draw resource: ";
     uint iSel = input(1, 2);
     return (iSel == 1) ? ActionOrResource::ACTION : ActionOrResource::RESOURCES;
 }
 
-DiscardDecision HumanAgent::getDiscard(const State& state) {
+DiscardDecision StepByStepAgent::getDiscard(const State& state) {
     const std::vector<ActionCard>& cards = getMyPlayer(state).getActions();
     for(uint i=0; i<cards.size(); i++){
-        std::cout << '[' << i << "]: " << to_string(cards[i]) << '\n';
+        ostream() << '[' << i << "]: " << to_string(cards[i]) << '\n';
     }
-    std::cout << "Choose a card to discard: ";
+    ostream() << "Choose a card to discard: ";
     uint iSel = input(0, cards.size() - 1);
     return DiscardDecision( iSel );
 }
 
-MoveDecision HumanAgent::getMovement(const State& state, unsigned int) {
+MoveDecision StepByStepAgent::getMovement(const State& state, unsigned int) {
     const Character& charSel = chooseCharacter(state);
     std::vector<MoveDecision> possibleMovs = state.allMovementsForCharacter(charSel);
     for(uint i = 0; i<possibleMovs.size(); i++)
-        std::cout << '[' << i << "]: to " << possibleMovs[i].to << '\n';
-    std::cout << "Enter which position to select: ";
+        ostream() << '[' << i << "]: to " << possibleMovs[i].to << '\n';
+    ostream() << "Enter which position to select: ";
     uint iSel = input(0, possibleMovs.size() - 1);
     MoveDecision movSel = possibleMovs[iSel];
     return movSel;
 }
 
-ActionDecision HumanAgent::getAction(const State& state) {
+ActionDecision StepByStepAgent::getAction(const State& state) {
     ActionDecision ret{};
     const std::vector<ActionCard>& cards = getMyPlayer(state).getActions();
     if(cards.empty())
         return ActionDecision::pass();
     for(uint i=0; i<cards.size(); i++){
-        std::cout << '[' << (i+1) << "]: " << to_string(cards[i]) << '\n';
+        ostream() << '[' << (i+1) << "]: " << to_string(cards[i]) << '\n';
     }
-    std::cout << "Choose a card, any card (or 0 to skip): ";
+    ostream() << "Choose a card, any card (or 0 to skip): ";
     uint iSel = input(0, cards.size());
 
     if(iSel == 0)
@@ -93,18 +99,26 @@ ActionDecision HumanAgent::getAction(const State& state) {
         std::vector<std::array<const Character*, 2>> array;
         int i = 1;
         for(const auto& [ unit, enemies ] : allPossibleAttacks){
-            std::cout << unit->im.name << '\n';
+            ostream() << unit->im.name << '\n';
             for(const auto& enemy : enemies){
                 const char* startCharacter = (enemy == enemies.back()) ? "└" : "├";
-                std::cout << '[' << i << ']' << startCharacter << "─" << enemy->im.name << '\n';
+                ostream() << '[' << i << ']' << startCharacter << "─" << enemy->im.name << '\n';
                 array.push_back( { unit, enemy } );
                 i += 1;
             }
         }
-        std::cout << "Enter which attack to select: ";
-        uint iSel = input(0, i-1);
+        ostream() << "Enter which attack to select: (or 0 to skip)";
+        uint iSel = input(0, i);
+        if(i == 0) return ActionDecision::pass();
         ret.subjectPos = array[iSel - 1][0]->pos;
         ret.objectPos = array[iSel - 1][1]->pos;
     }
     return ret;
+}
+
+RandomAgent::RandomAgent(uint myId): StepByStepAgent(myId), generator(std::random_device()()) {
+}
+
+uint RandomAgent::input(uint min, uint max){
+    return std::uniform_int_distribution<uint>(min, max)(generator);
 }
--- src/agent.hpp
+++ src/agent.hpp
@@ -6,6 +6,8 @@
 #include "character.hpp"
 #include "deck.hpp"
 #include "decision.hpp"
+#include <iostream>
+#include <random>
 
 class State;
 
@@ -26,15 +28,38 @@ public:
     virtual unsigned int getSpecialAction(const State& state, Effect& effect) = 0;
 };
 
-class HumanAgent: public Agent {
-    const Character& chooseCharacter(const State& state) const;
+class StepByStepAgent: public Agent {
+    const Character& chooseCharacter(const State& state);
+protected:
+    virtual uint input(uint min, uint max) = 0;
+    virtual std::ostream& ostream() = 0;
 public:
-    HumanAgent(uint myId);
+    StepByStepAgent(uint myId) : Agent(myId) {};
     ActionOrResource getDrawAction(const State&) override;
     DiscardDecision getDiscard(const State&) override;
     MoveDecision getMovement(const State& state, unsigned int) override;
     ActionDecision getAction(const State& state) override;
     unsigned int getSpecialAction(const State& state, Effect& effect) override;
+};
+
+class HumanAgent: public StepByStepAgent {
+protected:
+    uint input(uint min, uint max) override;
+    std::ostream& ostream() override { return std::cout; }
+public:
+    HumanAgent(uint myId);
+};
+
+class RandomAgent: public StepByStepAgent {
+    std::minstd_rand generator;
+    class NoopOstream: public std::ostream {
+        template<typename T> std::ostream& operator<<(const T&){ /*do nothing*/ return *this; }
+    } myostream;
+protected:
+    uint input(uint min, uint max) override;
+    std::ostream& ostream() override { return myostream; }
+public:
+    RandomAgent(uint myId);
 };
 
 #endif //REVELATION_AGENT_HPP
