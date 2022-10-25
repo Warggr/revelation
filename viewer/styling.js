292ba3cd9475984193144c3c903e9bbd9329e5e0
Pierre Ballif <pierre.ballif@tum.de>
2022-10-25 20:44:57 +0200

    Add front-end to create teams and units

---
+++ viewer/styling.js
@@ -0,0 +1,18 @@
+function teamClass(iTeam){
+	return 'teamid-' + teamNames[iTeam].toLowerCase().replaceAll(' ', '-')
+}
+
+function characterFactory(char, iTeam){
+	var charDom = document.createElement('div');
+	charDom.id = 'cid-' + char.cid;
+	charDom.classList.add( teamClass(iTeam) );
+	charDom.classList.add('character');
+	charDom.classList.add('team-' + (iTeam+1));
+	charDom.innerHTML =
+		`<h1>${char.name}</h1>
+		 <div class="stats">
+		 <div class="HP">${char.HP}</div><div class="maxHP">${char.maxHP}</div>`+
+		`<div class="atk softAtk">${char.softAtk}</div><div class="atk hardAtk">${char.hardAtk}</div><div class="mov">${char.mov}</div><div class="rng">${char.rng}</div>`+
+		`<div class="netWorth">${char.netWorth}</div><i>${char.flavor}</i>`;
+	return charDom;
+}
